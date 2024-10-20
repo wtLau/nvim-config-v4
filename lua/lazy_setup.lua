675f066a0a688432342fb627b194177bf8071eef
Brian <26396307+wtLau@users.noreply.github.com>
2024-10-19 21:10:00 -0700

    move custom config to astrocore

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -3,21 +3,19 @@ require("lazy").setup({
   { import = "plugins" },
   {
     "AstroNvim/AstroNvim",
-    version = "^4", -- Remove version tracking to elect for nighly AstroNvim
+    version = "^4",                -- Remove version tracking to elect for nighly AstroNvim
     import = "astronvim.plugins",
-    opts = { -- AstroNvim options must be set here with the `import` key
-      mapleader = " ", -- This ensures the leader key must be configured before Lazy is set up
-      maplocalleader = ",", -- This ensures the localleader key must be configured before Lazy is set up
-      icons_enabled = true, -- Set to false to disable icons (if no Nerd Font is available)
-      pin_plugins = nil, -- Default will pin plugins when tracking `version` of AstroNvim, set to true/false to override
+    opts = {                       -- AstroNvim options must be set here with the `import` key
+      mapleader = " ",             -- This ensures the leader key must be configured before Lazy is set up
+      maplocalleader = ",",        -- This ensures the localleader key must be configured before Lazy is set up
+      icons_enabled = true,        -- Set to false to disable icons (if no Nerd Font is available)
+      pin_plugins = nil,           -- Default will pin plugins when tracking `version` of AstroNvim, set to true/false to override
       update_notifications = true, -- Enable/disable notification about running `:Lazy update` twice to update pinned plugins
       -- set to true or false etc.
-      relativenumber = false, -- sets vim.opt.relativenumber
-      number = true, -- sets vim.opt.number
-      signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-      wrap = true, -- sets vim.opt.wrap
-      scrolloff = 80, -- Number of lines to keep above and below the cursor
-      spell = false, -- sets vim.opt.spell
+      relativenumber = false,      -- sets vim.opt.relativenumber
+      number = true,               -- sets vim.opt.number
+      signcolumn = "auto",         -- sets vim.opt.signcolumn to auto
+      spell = false,               -- sets vim.opt.spell
     },
   },
 } --[[@as LazySpec]], {
